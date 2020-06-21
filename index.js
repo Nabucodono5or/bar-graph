7b2684e2650a9660a9e7fa9d14a8b8b0fed5a555
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-06-21 09:17:52 -0300

    primeiro data binding

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import { select } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { csv, json } from "d3-fetch";
 import { scaleLinear, scaleQuantile } from "d3-scale";
 import { nest } from "d3-collection";
+import { min, max, mean } from "d3-array";
 
 select("body").append("h1").text("Olá mundo");
 
+// --------------- Trabalhando com scales métodos (linear e quantile) ----------------
 // let newRamp = scaleLinear().domain([500000, 13000000]).range([0, 500]);
 // console.log(newRamp(1000000));
 // console.log(newRamp(9000000));
@@ -25,16 +27,51 @@ select("body").append("h1").text("Olá mundo");
 // console.log(newRampQuantile(29));
 // console.log(newRampQuantile(1000));
 
-csv(require("./data/cities.csv")).then((data) => {
-  console.log(data);
-});
+// --------------- Trabalhando com min, max e mean métodos ----------------
+// csv(require("./data/cities.csv")).then((data) => {
+//   console.log(data);
+//   let minimo = min(data, (el) => {
+//     return +el.population;
+//   });
+
+//   let maximo = max(data, (el) => {
+//     return +el.population;
+//   });
+
+//   let media = mean(data, (el) => {
+//     return +el.population;
+//   });
+
+//   console.log(minimo);
+//   console.log(maximo);
+//   console.log(media);
+// });
 
 let tweets = require("./data/tweets.json");
-let nestedTweets = nest()
-  .key((el) => {
-    return el.user;
-  })
-  .entries(tweets.tweets);
 
-console.log(tweets.tweets);
-console.log(nestedTweets);
+// --------------- Trabalhando com agrupamentos de dados usando nest método ----------------
+// let nestedTweets = nest()
+//   .key((el) => {
+//     return el.user;
+//   })
+//   .entries(tweets.tweets);
+
+// console.log(tweets.tweets);
+// console.log(nestedTweets);
+
+function dataViz(incomingData) {
+    select("body")
+    .selectAll("div.cities")
+    .data(incomingData)
+    .enter()
+    .append("div")
+    .attr("class", "cities")
+    .html((d, i) => {
+      return d.label;
+    });
+
+}
+
+csv(require("./data/cities.csv")).then((data) => {
+    dataViz(data);
+});
