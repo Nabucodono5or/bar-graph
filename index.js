ad6b6281910ea945f577db2f5d73fc2483202926
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-06-21 05:49:23 -0300

    data loading

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import { select } from 'd3-selection';
+import { select } from "d3-selection";
+import { csv, json } from "d3-fetch";
 
 select("body").append("h1").text("Olá mundo");
+
+csv(require("./data/cities.csv")).then((data) => {
+  console.log(data);
+});
+
+let tweets = require("./data/tweets.json");
+console.log(tweets.tweets);
